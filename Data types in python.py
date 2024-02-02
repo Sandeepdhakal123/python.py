58a9761ec204ed3c52b002e808c3eecd3bd3bc6b
Sandeep Dhakal <126345770+Sandeepdhakal123@users.noreply.github.com>
2024-02-02 01:45:20 -0700

    Add files via upload

---
+++ Data types in python.py
@@ -0,0 +1,16 @@
+"""
+** Data types in python **
+
+1. List data type.
+2. None Data type.
+3. Tuple data Type
+4. Set data type.
+5. Boolen data type.
+6. Dictionary Data type.
+7. Intiger Data type.
+8. Float Data type
+9. Range data type 
+
+They are the Data types used in Python 
+"""
+
