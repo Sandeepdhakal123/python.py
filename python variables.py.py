04a070be4af3b539f8420a9340b7717b9f4d6b9a
Sandeep Dhakal <126345770+Sandeepdhakal123@users.noreply.github.com>
2024-02-01 22:22:44 -0700

    Add files via upload

---
+++ python variables.py.py
@@ -0,0 +1,16 @@
+a=int(input('Enter the first number:'))
+b= int(input('Enter the second number:') )
+
+# calculating
+sum = a + b
+diff = a - b
+mod = a %b
+mul = a * b
+div = a /b
+# printing the output
+
+print(f'The sum  is {sum}')
+print(f'The diff is {diff}')
+print(f'The mod is {mod}')
+print(f'The mul is {mul}')
+print(f'The div is {div}')
