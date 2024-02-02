efd18790b369111a0c942b5481d6db62c6496413
Sandeep Dhakal <126345770+Sandeepdhakal123@users.noreply.github.com>
2024-02-02 01:37:59 -0700

    Add files via upload

---
+++ None Data type.py
@@ -0,0 +1,13 @@
+"""
+1. None Data type.
+   => The none data type mantain the empty state if the variable.
+
+
+"""
+# Eample of non data type
+result = None
+print(result) # Output is None
+def some_functions():
+    pass
+result = some_functions()
+print (result)
