f2e7df9450d168ba218ce405222a56031fb15acc
Sandeep Dhakal <126345770+Sandeepdhakal123@users.noreply.github.com>
2024-02-02 01:30:10 -0700

    Add files via upload

---
+++ Data types.py
@@ -0,0 +1,49 @@
+"""
+Data types in python 
+1. There are varieties of data type
+   a. None Data type.
+   => The none data type mantain the empty state if the variable 
+
+   b. List data type.
+   => List data type is the type if data type which is changable or you can modify 
+   List data type is used to store a collections of items which is defined using
+   Square brackets []
+
+   c. Tuple data Type.
+   => Tuple is the non changable data type when you write the code in code editor and you can't modify it.
+   Tuple is defined using parentheses.   () 
+
+   d. Set data type.
+   => The set data type used to store unordered data collection of unique elements.
+   it is denoted by curly bracket {}
+
+   e. Boolen data type.
+   => In python boolen data type shows truth values or we can say reasult (True or false).
+
+   f. Dictionary Data type. 
+    => In Python, a dictionary is a built-in data type that is used to store collections of key-value pairs.
+     Dictionaries are also known as associative arrays, maps, or hash maps in other programming languages. 
+     They are defined using curly braces {}, and the key-value pairs are separated by colons 
+
+   g. Intiger Data type.
+   => The integer data type is the type which study the intiger value for eg : 1,2,3,4,5,6
+
+
+   h. Float Data type
+   => In python float data type reads the decimal value of the function. for eg: 2.000, 3.1,290.00000020202002
+
+   i. Range data type 
+   => In Python, the range type is used to represent an immutable sequence of numbers, 
+"""
+
+# # For example of None Data type
+result = None 
+print(result) #output None 
+def some_function():
+    pass
+result = some_function()
+print(result) #output None
+
+#Example of list type
+numbers [1, 2, 3, 4, 5, 6, 7]
+print('numbers')
